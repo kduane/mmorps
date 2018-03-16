ae53c5cc84477297249ec728f44531d5ed242063
kduane <karl.duane@gmail.com>
2018-03-16 08:39:35 -0400

    created an empty results.erb view, added session counter to server

--- server.rb
+++ server.rb
@@ -7,3 +7,12 @@ set :bind, '0.0.0.0'  # bind to all interfaces
 use Rack::Session::Cookie, {
   secret: "keep_it_secret_keep_it_safe"
 }
+
+get '/' do
+  if session[:visit_count].nil?
+    session[:visit_count] = 1
+  else
+    session[:visit_count] += 1
+  end
+  erb :index
+end
